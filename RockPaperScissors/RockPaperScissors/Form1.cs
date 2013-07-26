06e01037bb28d8ee306b5dec4e2928f40b959b9e
floret <sorrowbedamned@gmail.com>
2013-07-26 20:17:57 +0200

    It works

--- RockPaperScissors/RockPaperScissors/Form1.cs
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -21,12 +21,12 @@ namespace RockPaperScissors
         {
 
         }
-        string P1Selection="";
+        string P1Selection = "";
         private void cmbPlayerOne_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (cmbPlayerOne.SelectedIndex == 0) { P1Selection = "Rock"; }
             else if (cmbPlayerOne.SelectedIndex == 1) { P1Selection = "Paper"; }
-            else if (cmbPlayerOne.SelectedIndex == 2) { P1Selection = "Scissors"; }            
+            else if (cmbPlayerOne.SelectedIndex == 2) { P1Selection = "Scissors"; }
         }
         string P2Selection = "";
         private void cmbPlayerTwo_SelectedIndexChanged(object sender, EventArgs e)
@@ -39,8 +39,16 @@ namespace RockPaperScissors
         private void btnGo_Click(object sender, EventArgs e)
         {
             if (P1Selection == P2Selection) { lvwDisplay.Items.Add("Its a draw!"); }
-
+            //P1 Wins
+            if (P1Selection == "Rock" && P2Selection == "Scissors") { lvwDisplay.Items.Add("Player 1 Wins!"); }
+            else if (P1Selection == "Paper" && P2Selection == "Rock") { lvwDisplay.Items.Add("Player 1 Wins!"); }
+            else if (P1Selection == "Scissors" && P2Selection == "Paper") { lvwDisplay.Items.Add("Player 1 Wins!"); }
+            //P2 Wins
+            if (P2Selection == "Rock" && P1Selection == "Scissors") { lvwDisplay.Items.Add("Player 2 Wins!"); }
+            else if (P2Selection == "Paper" && P1Selection == "Rock") { lvwDisplay.Items.Add("Player 2 Wins!"); }
+            else if (P2Selection == "Scissors" && P1Selection == "Paper") { lvwDisplay.Items.Add("Player 2 Wins!"); }
+            //
         }
-        
+
     }
 }
